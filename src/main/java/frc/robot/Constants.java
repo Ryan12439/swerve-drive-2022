560ab3d92f844a22b00ceb47320df742f077771e
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2022-01-15 20:43:58 -0600

    Add some constants and swerve drive lib

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,42 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    // CAN Bus IDs
+
+    // Front Left Drive Motor
+    public static final int FL_MODULE_DM = 13;
+    // Front Left Steer Motor
+    public static final int FL_MODULE_SM = 14;
+    // Front Left Steer Encoder
+    public static final int FL_MODULE_SE = 3;
+
+    // Front Right Drive Motor
+    public static final int FR_MODULE_DM = 15;
+    // Front Right Steer Motor
+    public static final int FR_MODULE_SM = 16;
+    // Front Right Steer Encoder
+    public static final int FR_MODULE_SE = 4;
+
+    // Back Left Drive Motor
+    public static final int BL_MODULE_DM = 11;
+    // Back Left Steer Motor
+    public static final int BL_MODULE_SM = 12;
+    // Back Left Steer Encoder
+    public static final int BL_MODULE_SE = 2;
+
+    // Back Right Drive Motor
+    public static final int BR_MODULE_DM = 17;
+    // Back Right Steer Motor
+    public static final int BR_MODULE_SM = 18;
+    // Back Right Steer Encoder
+    public static final int BR_MODULE_SE = 5;
+
+    
+
+    // Offsets
+    public static final double FL_STEER_OFFSET = -Math.toRadians(71.279297+0.25);
+    public static final double FR_STEER_OFFSET = -Math.toRadians(7.294922+1);
+    public static final double BL_STEER_OFFSET = -Math.toRadians(290.214844+0.25);
+    public static final double BR_STEER_OFFSET = -Math.toRadians(225.263672+0.75);
+}
